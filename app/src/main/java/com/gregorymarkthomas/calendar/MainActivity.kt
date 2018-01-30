e1851c4f2148cf2a3f2b66d21221f3beaf8caadc
Gregory Thomas <greg.thomas@ziconix.com>
2018-01-30 16:37:17 +0000

    Working on a nice way to attach and detach child views to the activity. Needs completing.

--- app/src/main/java/com/gregorymarkthomas/calendar/MainActivity.kt
+++ app/src/main/java/com/gregorymarkthomas/calendar/MainActivity.kt
@@ -3,17 +3,22 @@ package com.gregorymarkthomas.calendar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import com.gregorymarkthomas.calendar.util.backstack.BackStack
 import com.gregorymarkthomas.calendar.util.backstack.BackStackCallback
 import com.gregorymarkthomas.calendar.util.backstack.BackStackInterface
 import com.gregorymarkthomas.calendar.view.MonthView
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity(), BackStackCallback {
     private var backstack: BackStackInterface = BackStack.getInstance(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        backstack.goTo(MonthView(this))
+        setContentView(R.layout.activity_main)
+
+        backstack.goTo(MonthView.newInstance(this, main_content))
     }
 
     override fun onDestroy() {
@@ -29,6 +34,19 @@ class MainActivity: AppCompatActivity(), BackStackCallback {
     }
 
     override fun onViewChanged(view: View) {
-        setContentView(view)
+//        main_content.removeViewAt(0)
+
+        main_content.setOnHierarchyChangeListener(object: ViewGroup.OnHierarchyChangeListener {
+            override fun onChildViewRemoved(parent: View?, child: View?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildViewAdded(parent: View?, child: View?) {
+                child.onInflated
+            }
+
+        })
+        main_content.addView(view)
     }
+
 }
--- app/src/main/java/com/gregorymarkthomas/calendar/view/MonthView.kt
+++ app/src/main/java/com/gregorymarkthomas/calendar/view/MonthView.kt
@@ -2,23 +2,42 @@ package com.gregorymarkthomas.calendar.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import com.gregorymarkthomas.calendar.R
 import com.gregorymarkthomas.calendar.presenter.CalendarPresenter
 import com.gregorymarkthomas.calendar.presenter.CalendarPresenterInterface
 
 import kotlinx.android.synthetic.main.month_view.view.*
 
-class MonthView(context: Context): View(context), CalendarViewInterface, View.OnClickListener {
+class MonthView: View, CalendarViewInterface, View.OnClickListener {
 
+    private val TAG = "MonthView"
     private var presenter: CalendarPresenterInterface = CalendarPresenter(this)
 
-    init {
-        inflate(context, R.layout.month_view, null)
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of MonthView.
+         */
+        fun newInstance(context: Context, viewGroup: ViewGroup): MonthView = MonthView(context, viewGroup)
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+    constructor(context: Context, viewGroup: ViewGroup) : super(context) {
+        inflate(context, R.layout.month_view, viewGroup)
+    }
+
+//    init {
+//        inflate(context, R.layout.month_view, viewGroup)
+//    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
         presenter.onViewCreated()
     }
 
