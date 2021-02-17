dd16ab3aa2a095ef84d692de39fdb49aa4309507
Gregory Mark Thomas <gregory@gregorymarkthomas.com>
2021-02-17 14:04:51 +0000

    Removed CalendarRepository interface as it's not really needed (and the model code is a little confusing)

--- app/src/main/java/com/gregorymarkthomas/calendar/model/AndroidCalendarRepository.kt
+++ app/src/main/java/com/gregorymarkthomas/calendar/model/AndroidCalendarRepository.kt
@@ -6,9 +6,9 @@ import com.gregorymarkthomas.calendar.model.interfaces.Resolver
 import com.gregorymarkthomas.calendar.presenter.CalendarPermission
 import com.gregorymarkthomas.calendar.presenter.contracts.AndroidPermissionContract
 
-class AndroidCalendarRepository(private val contentResolver: Resolver, private val permissionContract: AndroidPermissionContract): CalendarRepository, NeedsPermission(permissionContract) {
+class AndroidCalendarRepository(private val contentResolver: Resolver, private val permissionContract: AndroidPermissionContract): NeedsPermission(permissionContract) {
 
-    override fun getCalendars(callback: Callback.GetCalendarsCallback) {
+    fun getCalendars(callback: Callback.GetCalendarsCallback) {
         requestPermissions(object: OnAllGranted {
             override fun onAllGranted() {
                 callback.onGetCalendars(AndroidCalendarRetriever(contentResolver).get())
@@ -20,7 +20,7 @@ class AndroidCalendarRepository(private val contentResolver: Resolver, private v
      * This function retrieves the Days that (could) contain Events. The inputs are used to retrieve a span of days in a month and year.
      * This needs to take the CalendarProvider calendar data (via the cursor) and convert it into a list of the app's 'AppDay' objects
      */
-    override fun getEvents(fromDayInMonth: Int, month: Int, year: Int, specifiedNoOfDays: Int, callback: Callback.GetEventsCallback, calendarsToShow: IntArray) {
+    fun getEvents(fromDayInMonth: Int, month: Int, year: Int, specifiedNoOfDays: Int, callback: Callback.GetEventsCallback, calendarsToShow: IntArray) {
         requestPermissions(object: OnAllGranted {
             override fun onAllGranted() {
                 callback.onGetEvents(AndroidCalendarEventResolver(contentResolver).get(fromDayInMonth, month, year, specifiedNoOfDays, calendarsToShow))
--- app/src/main/java/com/gregorymarkthomas/calendar/model/CalendarRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.gregorymarkthomas.calendar.model
-
-/**
- * Calendar data could come from Android's Calendar Provider, or perhaps the Google Calendar API.
- */
-interface CalendarRepository {
-    fun getEvents(fromDayInMonth: Int, month: Int, year: Int, specifiedNoOfDays: Int, callback: Callback.GetEventsCallback, calendarsToShow: IntArray)
-    fun getCalendars(callback: Callback.GetCalendarsCallback)
-}
