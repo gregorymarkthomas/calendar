6802b30fd22a45a6af080e2ea39e7b0ad5d77225
Gregory Mark Thomas <gregory@gregorymarkthomas.com>
2021-02-18 20:34:42 +0000

    Renamed IntArraySharedPreferenceOptionData to String(etc) as IntArrays in SharedPreferences are stored as comma-separated strings anyway. Will use this OptionData class to store the saved calendar account.

--- app/src/main/java/com/gregorymarkthomas/calendar/model/interfaces/OptionInterface.kt
+++ app/src/main/java/com/gregorymarkthomas/calendar/model/interfaces/OptionInterface.kt
@@ -2,11 +2,8 @@ package com.gregorymarkthomas.calendar.model.interfaces
 
 object Option {
 
-    /**
-     * Data is stored as a comma-separated-String
-     */
-    interface IntArrayOption {
+    interface StringOption {
         fun getData(): String?
-        fun setData(commaSeparatedStr: String)
+        fun setData(str: String)
     }
 }
