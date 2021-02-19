13ac62d6d3404cc707f8ded58bf10c5107c69a5b
Gregory Mark Thomas <gregory@gregorymarkthomas.com>
2021-02-19 19:24:46 +0000

    Renamed repository to calendarRepo in Model in preparation for adding backendRepo property

--- app/src/main/java/com/gregorymarkthomas/calendar/model/Model.kt
+++ app/src/main/java/com/gregorymarkthomas/calendar/model/Model.kt
@@ -3,7 +3,7 @@ package com.gregorymarkthomas.calendar.model
 import com.gregorymarkthomas.calendar.util.interfaces.GetSharedPreferencesInterface
 import java.util.*
 
-class Model(private val repository: CalendarRepository, preferences: GetSharedPreferencesInterface): ModelInterface {
+class Model(private val calendarRepo: CalendarRepository, preferences: GetSharedPreferencesInterface): ModelInterface {
     private val calendarVisibiltyOption = VisibleCalendarsOption(preferences)
 
     override fun getTodayDate(): Date {
@@ -12,15 +12,15 @@ class Model(private val repository: CalendarRepository, preferences: GetSharedPr
     }
 
     override fun getEvents(account: AppAccount, dayInMonth: Int, month: Int, year: Int, numberOfDays: Int, callback: Callback.GetEventsCallback, calendarsToShow: IntArray) {
-        repository.getEvents(account, dayInMonth, month, year, numberOfDays, callback, calendarsToShow)
+        calendarRepo.getEvents(account, dayInMonth, month, year, numberOfDays, callback, calendarsToShow)
     }
 
     override fun getCalendars(account: AppAccount, callback: Callback.GetCalendarsCallback) {
-        repository.getCalendars(account, callback)
+        calendarRepo.getCalendars(account, callback)
     }
 
     override fun getAvailableAccounts(callback: Callback.GetAccountsCallback) {
-        repository.getAccounts(callback)
+        calendarRepo.getAccounts(callback)
     }
 
     override fun getSavedAccount(): AppAccount? {
