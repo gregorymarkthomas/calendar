06663b3f49abee36cf9c2d843024289c26f5f9ba
Gregory Mark Thomas <gregory@gregorymarkthomas.com>
2021-04-08 20:25:07 +0100

    Removed unnecessary mocked classes for ModelTest; I was having trouble getting past 'no answer found for: CalendarRepository.getEvents(...)' when I was trying to 'mockk()' all the callbacks, and I was battling a mockk verify() issue when I was manually mocking my callbacks. In the end, I had to re-use relaxUnitFun=true when mocking CalendarRepository because of course CalendarRepository.getEvents() (and all other functions so far) return Unit (i.e. nothing); mockk was expecting a result so I had to tell it to not.

--- app/src/test/java/com/gregorymarkthomas/calendar/model/ModelTest.kt
+++ app/src/test/java/com/gregorymarkthomas/calendar/model/ModelTest.kt
@@ -1,7 +1,7 @@
 package com.gregorymarkthomas.calendar.model
 
-import com.gregorymarkthomas.calendar.mock.MockCalendarRepository
 import com.gregorymarkthomas.calendar.model.repository.AccountRepository
+import com.gregorymarkthomas.calendar.model.repository.CalendarRepository
 import com.gregorymarkthomas.calendar.model.repository.CalendarVisibilityRepository
 import io.mockk.clearMocks
 import io.mockk.mockk
@@ -11,16 +11,19 @@ import org.junit.jupiter.api.BeforeEach
 
 class ModelTest {
 
-    private val calendarRepo = MockCalendarRepository()
+    /**
+     * 'relaxUnitFun = true' will stop 'io.mockk.MockKException: no answer found for...' error; "allows creation with no specific behaviour for Unit function".
+     *  We use callbacks in Model, and they all return Unit...
+     */
+    private val calendarRepo = mockk<CalendarRepository>(relaxUnitFun=true)
     private val accountRepo: AccountRepository = mockk()
     private val calendarVisibilityRepo: CalendarVisibilityRepository = mockk()
-    private val getEventsProperties: GetEventProperties = mockk()
-    private val getEventsCallback: ModelCallback.GetEventsCallback = mockk()
-
+    private val getEventsProperties = mockk<GetEventProperties>()
+    private val getEventsCallback = mockk<ModelCallback.GetEventsCallback>()
 
     @BeforeEach
     fun init() {
-        clearMocks(accountRepo, calendarVisibilityRepo, getEventsProperties, getEventsCallback)
+        clearMocks(accountRepo, calendarVisibilityRepo, getEventsProperties)
     }
 
     @Test
@@ -33,4 +36,5 @@ class ModelTest {
         // then
         verify { calendarRepo.getEvents(getEventsProperties, getEventsCallback) }
     }
+
 }
